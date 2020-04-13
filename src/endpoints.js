234e62fb9cffb3ecdc6ab4a3724b4009f83cdd24
an-ska <1.anna.piotrowska@gmail.com>
2020-04-13 12:35:46 +0200

    Update endpoints

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,7 +1,6 @@
 import { yesterdayFormattedDate } from './services/time';
 
-const API = 'http://data.fixer.io/api/';
-const API_KEY = '0e3e3d5bddeb34067fe3afdeab8e0f38';
+export const API = 'http://data.fixer.io/api/';
+export const API_KEY = '0e3e3d5bddeb34067fe3afdeab8e0f38';
 
-export const historicalRates = `${API}${yesterdayFormattedDate}?access_key=${API_KEY}`;
-export const latest = `${API}latest?access_key=${API_KEY}`;
+export const latest = `${API}latest?ac${yesterdayFormattedDate}cess_key=${API_KEY}`;
