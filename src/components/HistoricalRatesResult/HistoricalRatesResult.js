562cbf53e0f5e9592de26af7e552aa0965057b0e
an-ska <1.anna.piotrowska@gmail.com>
2020-04-14 20:52:22 +0200

    Show currency rate difference

--- src/components/HistoricalRatesResult/HistoricalRatesResult.js
+++ src/components/HistoricalRatesResult/HistoricalRatesResult.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { calculateCurrencyDifference } from '../../services/currency/currencyConverter';
 import PropTypes from 'prop-types';
 
-const HistoricalRatesResult = ({ historicalRates }) => (
-	<section className="container">
-		<p className="container__text">
-			Historical rates of currency: {historicalRates.currency}
-		</p>
-		{historicalRates.chart.map((data, index) => (
-			<p key={index} className="container__text">
-				<span>{data.date}</span>: <span>{data.rate}</span>
+const HistoricalRatesResult = ({ historicalRates }) => {
+	const currencyDifference = calculateCurrencyDifference(
+		historicalRates.chart[0].rate,
+		historicalRates.chart[1].rate,
+	);
+
+	return (
+		<section className="container">
+			<p className="container__text">
+				Historical rates of currency: {historicalRates.currency}
 			</p>
-		))}
-	</section>
-);
+			{historicalRates.chart.map((data, index) => (
+				<p key={index} className="container__text">
+					<span>{data.date}</span>: <span>{data.rate}</span>
+				</p>
+			))}
+			<p className="container__text">
+				{currencyDifference > 0 ? 'Rate decreased by: ' : 'Rate increased by: '}
+				{currencyDifference}
+			</p>
+		</section>
+	);
+};
 
 HistoricalRatesResult.propTypes = {
 	historicalRates: PropTypes.object.isRequired,
--- src/services/currency/currencyConverter.js
+++ src/services/currency/currencyConverter.js
@@ -1,2 +1,4 @@
 export const formatCurrency = amount => amount.toFixed(2);
 export const convertCurrency = (amount, rate) => formatCurrency(amount * rate);
+export const calculateCurrencyDifference = (firstAmount, secondAmount) =>
+	formatCurrency(Math.abs(firstAmount - secondAmount));
